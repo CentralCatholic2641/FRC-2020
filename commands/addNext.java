bd8888503d5dc1097cf45464d70b6619617d9030
CentralCatholic2641 <59667023+CentralCatholic2641@users.noreply.github.com>
2020-01-23 15:54:30 -0500

    Test

--- commands/addNext.java
+++ commands/addNext.java
@@ -18,6 +18,7 @@ public class addNext extends CommandGroupBase {
     // e.g. addSequential(new Command1());
     // addSequential(new Command2());
     // these will run in order.
+    addSequential(new Command1());
     
 
     // To run multiple commands at the same time,
