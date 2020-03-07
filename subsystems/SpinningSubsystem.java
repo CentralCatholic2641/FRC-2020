3b13bbde113e36e05efd703e367e6d0907731d35
JJTomko1 <JJTomko1@gmail.com>
2020-03-07 14:06:34 -0500

    color sensor

--- subsystems/SpinningSubsystem.java
+++ subsystems/SpinningSubsystem.java
@@ -25,11 +25,28 @@ public class SpinningSubsystem extends SubsystemBase {
   public DoubleSolenoid spinngingPiston = new DoubleSolenoid(Constants.spinnerPnuematicPort1, Constants.spinnerPnuematicPort2);
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
   private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  String convertedColor;
+  Color rgbColor;
 
   //Reads the color sensor values and prints them to the terminal
   public void readColorSensor(){
     Color detectedColor = m_colorSensor.getColor();
-    System.out.println(detectedColor);
+    convertedColor = detectedColor.toString();
+    rgbColor = hex2Rgb(detectedColor.toString());
+    System.out.println("Detected: " + detectedColor);
+    System.out.println("Converted Color: " + convertedColor);
+    System.out.println("RGB: " + rgbColor);
+
+
+  }
+
+  public static Color hex2Rgb(String colorStr) {
+    //This takes a string, If it is hexidemal it will parse the string
+    //The number will be converted to an integer by dividing by 16
+    return new Color(
+            Integer.valueOf( colorStr.substring( 1, 3 ), 16 ),
+            Integer.valueOf( colorStr.substring( 3, 5 ), 16 ),
+            Integer.valueOf( colorStr.substring( 5, 7 ), 16 ) );
   }
 
   public SpinningSubsystem() {
